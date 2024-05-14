70fbb2da7fa128f392fb159b2d3abf3d39be53e6
ahmedzaki98 <ahmedzaki19998@gmail.com>
2024-05-15 00:19:10 +0300

    handle toast

--- src/router.js
+++ src/router.js
@@ -1,45 +1,54 @@
 import { ToastContainer } from "react-toastify";
 import Navbar from "./layout/Navbar";
 import Header from "./layout/Header";
-import { Outlet, Route, ScrollRestoration, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Outlet,
+  Route,
+  ScrollRestoration,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Footer from "./layout/Footer/Footer";
 import Shop from "./pages/shop/Shop";
 import ProductDetails from "./pages/productDetails/ProductDetails";
 import Cart from "./pages/cart/Cart";
 import AddProduct from "./pages/add-product/AddProduct";
+import { useTranslation } from "react-i18next";
 
 const Layout = () => {
-    return (
-      <div>
-        <ToastContainer
-          position="top-center"
-          autoClose={1000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-        <Navbar />
-        <Header />
-        <ScrollRestoration />
-        <Outlet />
-        <Footer />
-      </div>
-    );
-  };
-  export const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Shop />}></Route>
-          <Route path="/product/:_id" element={<ProductDetails />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-          <Route path="/add-product" element={<AddProduct />}></Route>
-        </Route>
-      </Route>
-    )
+  const { i18n } = useTranslation();
+
+  return (
+    <div>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={i18n.language === "en" ? false : true}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <Navbar />
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+    </div>
   );
+};
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Shop />}></Route>
+        <Route path="/product/:_id" element={<ProductDetails />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/add-product" element={<AddProduct />}></Route>
+      </Route>
+    </Route>
+  )
+);
